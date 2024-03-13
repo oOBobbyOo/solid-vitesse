4f54e6f864481ea3826d48c12652ecb3525f9ded
oOBobbyOo <944831575@qq.com>
2024-03-13 12:22:08 +0800

    feat: devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
-import { resolve } from 'node:path'
+// @see https://github.com/thetarnav/solid-devtools
+import devtools from 'solid-devtools/vite'
 
 // @see https://unocss.dev/integrations/vite
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-  plugins: [
-    solid(), 
-    UnoCSS()
-  ],
+  plugins: [devtools(), solid(), UnoCSS()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
